73fbacab430c784be9debd43d406f5f1b4993f2d
Diogo Cezar <>
2022-05-23 10:57:58 -0300

    feat: tp-000 including rule of tp-XXX on commit message

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,21 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  plugins: ['commitlint-plugin-function-rules'],
+  rules: {
+    'scope-enum': [0],
+    'function-rules/scope-enum': [
+      2,
+      'always',
+      (parsed) => {
+        // Standard tp-1/type/message
+        const { header: commitMessage } = parsed
+        const [tpId] = commitMessage.split('/')
+        const tpIdRegex = /tp-([0-9]+)/g
+        if (!tpIdRegex.test(tpId)) {
+          return [false, 'You need to fill TP card code.']
+        }
+        return [true]
+      },
+    ],
+  },
+}
